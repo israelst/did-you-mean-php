d8bd7557ebc50af439d15c1370d5ce8bfb092138
Israel Teixeira <israelst@gmail.com>
2012-11-21 08:51:17 -0200

    Comparing builtin distances outputs

---
+++ edition.php
@@ -0,0 +1,9 @@
+<?php 
+
+print "\n\Builtin distances:\n";
+print "\n>> similar_text('casa', 'caso');\n";
+print similar_text('casa', 'caso');
+print "\n>> levenshtein('casa', 'caso');\n";
+print levenshtein('casa', 'caso');
+
+
